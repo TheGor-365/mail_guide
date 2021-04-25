8a6c4734df1e556021ddf9413e65e3cecf3c3dd8
Gor <7495500@gmail.com>
2021-04-25 23:17:42 +0300

    Add REST structure to controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,55 @@
 class UsersController < ApplicationController
-  # POST /users
-  # POST /users.json
+
+  before_action :set_user, only: [ :show, :edit, :update, :destroy ]
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show; end
+  def edit; end
+
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
 
     respond_to do |format|
       if @user.save
         # Tell the UserMailer to send a welcome email after save
         UserMailer.with(user: @user).welcome_email.deliver_later
 
-        format.html { redirect_to(@user, notice: 'User was successfully created.') }
-        format.json { render json: @user, status: :created, location: @user }
+        format.html { redirect_to @user }
       else
         format.html { render action: 'new' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
+
+  def update
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :login)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
